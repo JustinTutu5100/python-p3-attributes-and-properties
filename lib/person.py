693f54449a6f88e7ae3e014e974622f468063f3a
justintutu5100 <devolostutu@gmail.com>
2025-08-14 09:06:41 +0300

    modified work

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    approved_jobs = ["Sales", "Engineer", "Manager", "Chef", "ITC"]
+
+    def __init__(self, name=None, job=None):
+        if name is not None:
+            if isinstance(name, str) and 1 <= len(name) <= 25:
+                self.name = name.title()
+            else:
+                print("Name must be string between 1 and 25 characters.")
+                self.name = None
+        else:
+            self.name = None
+
+        if job is not None:
+            if job in self.approved_jobs:
+                self.job = job
+            else:
+                print("Job must be in list of approved jobs.")
+                self.job = None
+        else:
+            self.job = None
